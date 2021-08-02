e3ac403b2d3f4e32c1593135cb0f575c5a2588bd
thang <vanthang@gmail.com>
2021-08-02 10:55:40 +0700

    init project

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function hello(Request $req){
+        dd("Hello");
+    }
+}
